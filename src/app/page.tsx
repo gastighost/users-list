fb5fa53f7d93ce74509c57db43a9d933fc5ea966
Gaston Roxas <gacaro51597@gmail.com>
2023-07-13 00:39:30 +0800

    add proper page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import UserList from "./components/UserList";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <UserList />
+    <div className="flex justify-center items-center h-screen">
+      <Link href="/users">
+        <button className="text-blue-500 hover:text-white bg-transparent hover:bg-blue-500 text-lg font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Enter Users Page
+        </button>
+      </Link>
     </div>
   );
 }
